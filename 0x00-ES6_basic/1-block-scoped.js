eb077137c78fac84e4da1820a5b9d123ef6b4bee
grayolaRSA <gtndanga@gmail.com>
2023-10-02 20:30:25 +0200

    update

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse) {
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+    return [task2, task];
   }
+
+  return [task, task2];
+}
