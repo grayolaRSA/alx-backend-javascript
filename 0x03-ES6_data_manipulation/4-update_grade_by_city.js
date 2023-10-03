5e3589e3ef5167b19e4165c3b75979504e7038e5
grayolaRSA <gtndanga@gmail.com>
2023-10-03 22:38:36 +0200

    update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,6 @@
-import getStudentsByLocation from './2-get_students_by_loc';
-
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  // Get students in the specified city
-  const studentsInCity = getStudentsByLocation(students, city);
+  const studentsInCity = students.filter((student) => student.location === city);
 
-  // Map through the students to update their grades
   const updatedStudents = studentsInCity.map((student) => {
     const newGrade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
     if (newGrade) {
