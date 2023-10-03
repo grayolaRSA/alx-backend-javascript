5c2e928b0e671528d474c2650158cc3df48a511d
grayolaRSA <gtndanga@gmail.com>
2023-10-03 20:06:19 +0200

    update

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,5 @@
 export default function getStudentsByLocation(students, city) {
-  const filterOutput = students.filter((student) => student.city === city);
+  const filterOutput = students.filter((student) => student.location === city);
+
   return filterOutput;
 }
