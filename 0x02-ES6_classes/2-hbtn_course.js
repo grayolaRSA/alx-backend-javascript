8bba4c0d80371b6435adf500aea308d68c023446
grayolaRSA <gtndanga@gmail.com>
2023-10-08 17:13:08 +0200

    update

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,9 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
+      throw new Error('Invalid attribute types');
+    }
+
     this._name = name;
     this._length = length;
     this._students = students;
