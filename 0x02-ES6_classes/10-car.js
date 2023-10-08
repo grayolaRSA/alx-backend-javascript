ef8c20a233a6c17547dc68b80e80030163bcbe0a
grayolaRSA <gtndanga@gmail.com>
2023-10-08 23:26:07 +0200

    update

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -3,25 +3,28 @@ const _motor = Symbol('motor');
 const _color = Symbol('color');
 
 export default class Car {
-  constructor(brand, motor, color) {
+    constructor(brand, motor, color) {
     this[_brand] = brand;
     this[_motor] = motor;
     this[_color] = color;
   }
-
   get brand() {
-    return this[_brand];
+    return this._brand;
   }
 
   get motor() {
-    return this[_motor];
+    return this._motor;
   }
 
   get color() {
-    return this[_color];
+    return this._color;
   }
 
   cloneCar() {
-    return new Car(this[_brand], this[_motor], this[_color]);
+    // Get the constructor of the current instance (Car or its subclass)
+    const Constructor = this.constructor;
+
+    // Create a new instance using the constructor
+    return new Constructor(this._brand, this._motor, this._color);
   }
 }
