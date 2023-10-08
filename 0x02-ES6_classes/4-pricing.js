ce3cb706e8d208177d1a91797492e4ea9dbf3c51
grayolaRSA <gtndanga@gmail.com>
2023-10-08 18:32:06 +0200

    update

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount === 'number') {
+      this._amount = newAmount;
+    } else {
+      throw new Error('amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (newCurrency instanceof Currency) {
+      this._currency = newCurrency;
+    } else {
+      throw new Error('not a valid currency');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
