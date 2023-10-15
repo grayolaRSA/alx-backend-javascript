73b6f825b73bc34306f80a4336d88fcb5342887c
grayolaRSA <gtndanga@gmail.com>
2023-10-15 15:42:51 +0200

    update

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,14 +1,16 @@
-function getResponseFromAPI () {
-    const myPromise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const data = "Thats all folks!";
-            resolve(data);
-        }, 1000);
-    });
+function getResponseFromAPI() {
+  const myPromise = new Promise((resolve) => {
+    setTimeout(() => {
+      const data = 'Thats all folks!';
+      resolve(data);
+    }, 1000);
+  });
 
-    myPromise.then((result) => {
-        console.log(result);
-    }).catch((err) => {
-        console.error(err);
-    });
+  myPromise.then((result) => {
+    console.log(result);
+  }).catch((err) => {
+    console.error(err);
+  });
 }
+
+getResponseFromAPI();
