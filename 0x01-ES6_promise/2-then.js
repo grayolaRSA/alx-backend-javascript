d4321a4fb57d673259028d99cba6d62bad714143
grayolaRSA <gtndanga@gmail.com>
2023-10-15 16:56:57 +0200

    update

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      const data = {
-        status: 200,
-        body: 'success',
-      };
-      resolve(data);
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => {
       console.log('Got a response from the API');
-    } else {
-      const err = '';
-      reject(new Error(err));
-    }
-  });
+    });
 }
