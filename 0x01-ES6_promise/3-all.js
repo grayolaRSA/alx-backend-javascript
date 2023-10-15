939016194e9244d07dc0e5b3b053b6b5c0405a55
grayolaRSA <gtndanga@gmail.com>
2023-10-15 17:46:59 +0200

    update

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const photoPromise = uploadPhoto();
   const userPromise = createUser();
 
-  Promise.all([photoPromise, userPromise])
+  return Promise.all([photoPromise, userPromise])
     .then((results) => {
       const [photo, User] = results;
       console.log(`${photo.body} ${User.firstName} ${User.lastName}`);
