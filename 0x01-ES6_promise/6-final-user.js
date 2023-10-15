b9e0f10a9b15c8ab4ffe0746e2c65bdd719133c3
grayolaRSA <gtndanga@gmail.com>
2023-10-15 19:45:35 +0200

    update

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userSignUp = signUpUser(firstName, lastName);
+  const userPhoto = uploadPhoto(fileName);
+
+  return Promise.allSettled([userSignUp, userPhoto])
+    .then((result) => {
+      console.log(result.status);
+    });
+}
