c12d14569b81ca8aecbff6987737c04ad5eb6085
grayolaRSA <gtndanga@gmail.com>
2023-11-21 21:02:13 +0200

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,7 +6,10 @@ const rl = readline.createInterface({
 });
 
 rl.question('Welcome to Holberton School, what is your name?\n', (userInput) => {
+  // Assuming you want to print a personalized message based on the user's name
   console.log(`Your name is: ${userInput}`);
   console.log('This important software is now closing');
   rl.close();
 });
+
+module.exports = rl;
