10d74dc512a57e8f7d465c750f33079867d08a94
grayolaRSA <gtndanga@gmail.com>
2023-11-23 20:19:30 +0200

    update

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,31 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('test suite', () => {
+  let stub;
+
+  beforeEach(() => {
+    stub = sinon.stub(Utils, 'calculateNumber');
+    stub.returns(10);
+  });
+
+  afterEach(() => {
+    stub.restore();
+  });
+
+  it('stub the calculateNumber method and log correct msg', () => {
+    const consoleSpy = sinon.spy(console, 'log');
+    const a = 100;
+    const b = 20;
+
+    sendPaymentRequestToApi(a, b);
+
+    expect(stub.calledWith('SUM', a, b)).to.be.true;
+
+    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+
+    consoleSpy.restore();
+  });
+});
